702f3d41edcb4ad8a8180c3a2a7b7728e11b1038
Adaml <Adamlancaster.al@gmail.com>
2020-10-13 08:38:54 -0400

    ergaef

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+# practice scraping websites for info. 
 #
 #
-#
+
 import requests
+
+URL = "https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia"
+
+def main():
+
+    page = requests.get(URL)
+
+    print(page.content)
+
+
+
+
+
+
+
+
+
+    exit()
+
+main()
